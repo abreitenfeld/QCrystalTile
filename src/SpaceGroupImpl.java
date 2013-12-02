6786e9960ae5c256b096eb776ebbb6dfef5a0e61
EsGeh <SamuelGfroerer@googlemail.com>
2013-12-02 18:07:02 +0100

    added a (still untested!) implementation for Transformation.

---
+++ src/SpaceGroupImpl.java
@@ -0,0 +1,22 @@
+import java.util.Set;
+
+import interfaces.LatticeType;
+import interfaces.SpaceGroup;
+import interfaces.Transformation;
+
+
+public class SpaceGroupImpl implements SpaceGroup {
+
+	public SpaceGroupImpl() {
+	}
+
+	@Override
+	public LatticeType getLatticeType() {
+		return null;
+	}
+
+	@Override
+	public Set<Transformation> getTransformations() {
+		return null;
+	}
+}
---
+++ src/TransformationImpl.java
@@ -0,0 +1,54 @@
+
+import org.la4j.factory.CRSFactory;
+import org.la4j.vector.Vector;
+
+import interfaces.Matrix3D;
+import interfaces.Matrix4D;
+import interfaces.Transformation;
+import interfaces.Vector3D;
+
+
+public class TransformationImpl implements Transformation {
+
+	public TransformationImpl(Matrix3D linearPart, Vector3D translationPart) {
+		this.linearPart = linearPart;
+		this.translationPart = translationPart;
+	}
+	
+	public TransformationImpl(Matrix4D homogeneousM) {
+		this.linearPart = new Matrix3D(homogeneousM.sliceTopLeft(2, 2));
+		
+		this.translationPart = new Vector3D( homogeneousM.getColumn(3));
+	}
+
+	@Override
+	public Matrix3D linearPart() {
+		return linearPart;
+	}
+
+	@Override
+	public Vector3D translationPart() {
+		return translationPart;
+	}
+
+	@Override
+	public Matrix4D getAsHomogeneous() {
+		CRSFactory factory = new CRSFactory();
+		
+		Matrix4D ret = new Matrix4D(factory.createIdentityMatrix(4));
+		for( int i=0; i< 3; i++) {
+			Vector row = linearPart.getRow(i);
+			ret.setRow(i, row);
+		}
+		ret.setColumn(3, factory.createVector(new double[] { translationPart.get(0), translationPart.get(1), translationPart.get(2), 1 }));
+		return ret;
+	}
+
+	@Override
+	public Transformation composition(Transformation b) {
+		Matrix4D matr = new Matrix4D(this.getAsHomogeneous().multiply(b.getAsHomogeneous()));
+		return new TransformationImpl(matr);
+	}
+	private Matrix3D linearPart;
+	private Vector3D translationPart;
+}
