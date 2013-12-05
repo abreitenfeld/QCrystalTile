1500757494f81beafec98c49f52c23a37164fe1d
phobosultra <andre.breitenfeld@googlemail.com>
2013-12-05 16:49:43 +0100

    added interfaces for mesh and polygon and extended controller and view

---
+++ src/interfaces/Mesh.java
@@ -0,0 +1,19 @@
+package interfaces;
+
+import java.util.List;
+
+public interface Mesh {
+	
+	/**
+	* Returns a list of all vertices.
+	* @return
+	*/
+	public List<Vector3D> getVertices();
+	
+	/**
+	 * Returns a list of faces.
+	 * @return
+	 */
+	public List<Polygon> getFaces();
+	
+}
