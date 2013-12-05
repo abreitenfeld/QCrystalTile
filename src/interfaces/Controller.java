096a58e8f8e123d81e26cc5904d973a76afbcefb
phobosultra <andre.breitenfeld@googlemail.com>
2013-12-05 18:00:11 +0100

    added getter and setter for origin point in interface controller

--- src/interfaces/Controller.java
+++ src/interfaces/Controller.java
@@ -5,7 +5,9 @@ import java.util.EnumSet;
 public interface Controller {
 	Model getModel();
 	View getView();
-	
+
+	public Vector3D getOriginPoint();
+	public void setOriginPoint(Vector3D point);	
 	public void setViewOption(ViewOptions option, boolean value);
 	public boolean getViewOption(ViewOptions option);
 	
