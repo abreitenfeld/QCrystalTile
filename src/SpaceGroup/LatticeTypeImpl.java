3995ab9858013513c8b3a51b5933a7b754186f4a
EsGeh <SamuelGfroerer@googlemail.com>
2013-12-14 22:34:48 +0100

    added an implementation for LatticeType

---
+++ src/SpaceGroup/LatticeTypeImpl.java
@@ -0,0 +1,34 @@
+package SpaceGroup;
+
+import interfaces.LatticeType;
+
+public class LatticeTypeImpl implements LatticeType {
+
+	public LatticeTypeImpl(
+			String name,
+			CenteringType centeringType,
+			System system
+		) {
+			this.name = name;
+			this.centeringType = centeringType;
+			this.system = system;
+	}
+	
+	@Override
+	public String getName() {
+		return name;
+	}
+
+	@Override
+	public CenteringType getCenteringType() {
+		return centeringType;
+	}
+
+	@Override
+	public System getSystem() {
+		return system;
+	}
+	private String name;
+	private CenteringType centeringType;
+	private System system;
+}
