0acd60ee82b56b82ca41fd13b89e425de58324b3
EsGeh <SamuelGfroerer@googlemail.com>
2013-12-14 23:13:37 +0100

    adding a way to calculate a spaceGroup from a few basic transformations.

--- src/SpaceGroup/SpaceGroupImpl.java
+++ src/SpaceGroup/SpaceGroupImpl.java
@@ -1,5 +1,8 @@
 package SpaceGroup;
+import java.util.HashSet;
 import java.util.Set;
+
+import org.la4j.matrix.functor.MatrixFunction;
 
 import interfaces.LatticeType;
 import interfaces.SpaceGroup;
@@ -8,16 +11,69 @@ import interfaces.Transformation;
 
 public class SpaceGroupImpl implements SpaceGroup {
 
-	public SpaceGroupImpl() {
+	public SpaceGroupImpl(
+			LatticeType latticeType,
+			Set<Transformation> base
+		) {
+		this.latticeType = latticeType;
+		this.basicTransformations = base;
+		this.transformations = null;
 	}
 
 	@Override
 	public LatticeType getLatticeType() {
-		return null;
+		return latticeType;
 	}
 
 	@Override
 	public Set<Transformation> getTransformations() {
-		return null;
+		if(transformations == null)
+			closure();
+		return transformations;
 	}
+	
+	protected void closure() {
+		transformations = closure(basicTransformations);
+	}
+	
+	protected Set<Transformation> closure(Set<Transformation> base) {
+		Set<Transformation> res = new HashSet<Transformation>(base);
+		int iteration = 0;
+		int prevSize = 0;
+		while( res.size() > prevSize && cond(iteration, res.size())) {
+			prevSize = res.size();
+			res.addAll(
+					combine(res,base)
+				);
+			iteration ++;
+		};
+		return res;
+	}
+	
+	protected boolean cond(int iteration, int currentSize) {
+		return iteration < 6;
+	}
+	
+	protected Set<Transformation> combine( Set<Transformation> set, Set<Transformation> base) {
+		//List<Iterator<Transformation>> i = new ArrayList<Iterator<Transformation>>();
+		Set<Transformation> res = new HashSet<Transformation>(set);
+		for( Transformation t : set) {
+			for( Transformation b : base) {
+				Transformation newTrans = b.composition(t);
+				//System.out.println("adding\n" + newTrans.getAsHomogeneous());
+				MatrixFunction round = new MatrixFunction() {
+					public double evaluate(int row, int col, double entry) {
+						return Math.round(entry);
+					}
+				};
+				newTrans.getAsHomogeneous().update(round);
+				res.add(newTrans);
+			}
+		}
+		return res;
+	}
+	
+	private LatticeType latticeType;
+	private Set<Transformation> basicTransformations;
+	private Set<Transformation> transformations;
 }
