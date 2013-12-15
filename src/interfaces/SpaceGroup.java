d3429e801d746df24a3dc44731e84f4b13753ed0
EsGeh <SamuelGfroerer@googlemail.com>
2013-12-15 15:27:08 +0100

    a few more comments, improved SpaceGroup interface

--- src/interfaces/SpaceGroup.java
+++ src/interfaces/SpaceGroup.java
@@ -2,8 +2,14 @@ package interfaces;
 
 import java.util.Set;
 
+/*
+ * represents a space group
+ */
 public interface SpaceGroup {
 	LatticeType getLatticeType();
 	
+	// returns the set generating the group
+	Set<Transformation> getGeneratingSet();
+	// returns the closure of the generating set under the group operation (concatenation, that is)
 	Set<Transformation> getTransformations();
 }
