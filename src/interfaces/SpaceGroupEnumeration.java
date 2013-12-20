1e618292966345b1e8e4eae4545aedd5f1a87ad9
EsGeh <SamuelGfroerer@googlemail.com>
2013-12-20 17:51:26 +0100

    added some comments

--- src/interfaces/SpaceGroupEnumeration.java
+++ src/interfaces/SpaceGroupEnumeration.java
@@ -2,6 +2,9 @@ package interfaces;
 
 import java.util.List;
 
+/* attention:
+ * once created, an instance of an implementation of this interface is considered "immutable"!
+ */
 public interface SpaceGroupEnumeration<ID extends SpaceGroupID> extends List<ID> {
 
 }
