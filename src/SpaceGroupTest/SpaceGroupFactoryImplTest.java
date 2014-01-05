0b5773e74a671458cd7dddb2a0db80ce1dbfe1a3
davidd90 <d.damm90@gmx.de>
2014-01-05 20:20:57 +0100

    SpaceGroupFactory added

---
+++ src/SpaceGroupTest/SpaceGroupFactoryImplTest.java
@@ -0,0 +1,27 @@
+package SpaceGroupTest;
+
+import interfaces.InvalidSpaceGroupIDException;
+import interfaces.SpaceGroup;
+import interfaces.SpaceGroupFactory;
+import interfaces.SpaceGroupID;
+import interfaces.Transformation;
+
+import java.io.IOException;
+import java.util.Set;
+
+import InternationalShortSymbol.ID;
+import SpaceGroup.SpaceGroupImpl;
+import SpaceGroup.SpaceGroupFactoryImpl;
+
+public class SpaceGroupFactoryImplTest {
+	
+	public static void main(String[] args) throws IOException, InvalidSpaceGroupIDException {
+		ID key= new ID("I4132"); 
+		SpaceGroupFactoryImpl spgf=new SpaceGroupFactoryImpl();
+		SpaceGroup sp= spgf.createSpaceGroup(key);
+		Set<Transformation> set =sp.getGeneratingSet();
+		for (Transformation elem: set){
+			System.out.println(elem.getAsHomogeneous());
+		}
+	}
+}
