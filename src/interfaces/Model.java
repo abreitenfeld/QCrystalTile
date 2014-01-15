21db1dbddebd0d3b3e3cc07d52da44785ad7608e
phobosultra <andre.breitenfeld@googlemail.com>
2014-01-15 15:25:01 +0100

    added space group setter to model

--- src/interfaces/Model.java
+++ src/interfaces/Model.java
@@ -2,7 +2,7 @@ package interfaces;
 
 public interface Model {
 	public SpaceGroup getSpaceGroup();
-	//public setSpaceGroup(String name);
+	public setSpaceGroup(SpaceGroupID id);
 	
 	public Vector3D getPoint();
 	public void setPoint(Vector3D point);
