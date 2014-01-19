ad9f989d6451e24b36bda8c8bf6745b2c759b534
jk <ulukaiz@hotmail.com>
2014-01-19 18:17:57 +0100

    Merge Maven Template 2.0

---
+++ src/main/java/com/Softwareprojekt/app/App.java
@@ -0,0 +1,10 @@
+package com.Softwareprojekt.app;
+
+/**
+ * Created by jakob on 1/9/14.
+ */
+public class App {
+    public static void main(String[] args){
+        System.out.println("Hello World");
+    }
+}
