1ff32bdf0b0e685be6b729c3ee0420a781fd1f3c
davidd90 <d.damm90@gmx.de>
2014-02-04 12:36:20 +0100

    Lattice Types added

--- src/main/java/com/Softwareprojekt/interfaces/LatticeType.java
+++ src/main/java/com/Softwareprojekt/interfaces/LatticeType.java
@@ -6,6 +6,6 @@ public interface LatticeType {
 	public CenteringType getCenteringType();
 	public System getSystem();
 	
-	public enum CenteringType { P, F, I, C};
-	public enum System { TRICLINIC, MONOCLINIC, ORTHORHOMBIC, TETRAGONAL, RHOMBOHEDRAL, HEXAGONAL, CUBIC };
+	public enum CenteringType {A, P, F, I, C,R};
+	public enum System { TRICLINIC, MONOCLINIC, ORTHORHOMBIC, TETRAGONAL, RHOMBOHEDRAL, TRIGONAL, HEXAGONAL, CUBIC };
 }
