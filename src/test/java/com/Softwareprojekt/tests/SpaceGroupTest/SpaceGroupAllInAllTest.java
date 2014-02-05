1c40096d063f7ec4bba9f729b93b454a16f255f3
Samuel Gfrörer <SamuelGfroerer@googlemail.com>
2014-02-05 10:18:48 +0100

    improved the spaceGroupAllInAllTest

--- src/test/java/com/Softwareprojekt/tests/SpaceGroupTest/SpaceGroupAllInAllTest.java
+++ src/test/java/com/Softwareprojekt/tests/SpaceGroupTest/SpaceGroupAllInAllTest.java
@@ -18,13 +18,18 @@ public class SpaceGroupAllInAllTest {
 	public void test() throws FileNotFoundException, IOException, ParseException, InvalidSpaceGroupIDException {
 		SpaceGroupFactory factory = new SpaceGroupFactoryImpl();
 		SpaceGroupEnumeration<ID> sgEnum = new InternationalShortSymbolEnum();
-		int iSGIndex = 0;
-		for( ID id : sgEnum ) {
-			System.out.println("test loading spaceGroup " + iSGIndex +  ": \"" + id.stringRepr() + "\"" );
+		//int iSGIndex = 0;
+		//for( ID id : sgEnum ) {
+		for( int iSGIndex=0; iSGIndex<230; iSGIndex++ ) {
+			ID id = sgEnum.get(iSGIndex);
+			System.out.print("test loading spaceGroup " + (iSGIndex+1) +  ": \"" + id.stringRepr() + "\"" );
 			SpaceGroup sg = factory.createSpaceGroup(id);
+			System.out.println(" ... loaded");
+
 			// test if result is in the unit cell
 			Set<Transformation> transformations = sg.getTransformations();
-			iSGIndex ++;
+			System.out.println("\tcreators size: " + sg.getGeneratingSet().size());
+			System.out.println("\tsize: " + transformations.size());
 		}
 		//SpaceGroup sg = factory.createSpaceGroup(
 		//fail("Not yet implemented");
