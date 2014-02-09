f0bcc1797a40b6678d3010d682f3270dd2b05bee
davidd90 <d.damm90@gmx.de>
2014-02-09 14:38:43 +0100

    new methods added

--- src/main/java/com/Softwareprojekt/interfaces/SpaceGroupFactory.java
+++ src/main/java/com/Softwareprojekt/interfaces/SpaceGroupFactory.java
@@ -1,6 +1,11 @@
 package com.Softwareprojekt.interfaces;
 
+import java.util.Set;
+
+
 public interface SpaceGroupFactory<SpaceGroupKey extends SpaceGroupID> {
 	
 	SpaceGroup createSpaceGroup(SpaceGroupKey key) throws InvalidSpaceGroupIDException;
+	Set<SpaceGroupID> getIDbyCentering(LatticeType.CenteringType c);
+	Set<SpaceGroupID> getIDbySystem(LatticeType.System s);
 }
