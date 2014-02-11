5b6f301e6dbae4649d3358af288960a02ff8a5bb
davidd90 <d.damm90@gmx.de>
2014-02-11 18:17:26 +0100

    fixed generics

--- src/main/java/com/Softwareprojekt/interfaces/SpaceGroupFactory.java
+++ src/main/java/com/Softwareprojekt/interfaces/SpaceGroupFactory.java
@@ -2,11 +2,9 @@ package com.Softwareprojekt.interfaces;
 
 import java.util.Set;
 
-import com.Softwareprojekt.InternationalShortSymbol.ID;
-
 
 public interface SpaceGroupFactory<SpaceGroupKey extends SpaceGroupID> {
 	SpaceGroup createSpaceGroup(SpaceGroupKey key) throws InvalidSpaceGroupIDException;
-	Set<ID> getIDbyCentering(LatticeType.CenteringType c);
-	Set<ID> getIDbySystem(LatticeType.System s);
+	Set<SpaceGroupKey> getIDbyCentering(LatticeType.CenteringType c);
+	Set<SpaceGroupKey> getIDbySystem(LatticeType.System s);
 }
