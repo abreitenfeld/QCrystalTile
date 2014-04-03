b9834d174e5a8bb6b1fa3607497d7495812c1f10
Samuel Gfrörer <SamuelGfroerer@googlemail.com>
2014-04-03 13:15:55 +0200

    nicer comments

--- src/test/java/com/Softwareprojekt/tests/SpaceGroupTest/SpaceGroupFactoryImplTest.java
+++ src/test/java/com/Softwareprojekt/tests/SpaceGroupTest/SpaceGroupFactoryImplTest.java
@@ -107,7 +107,7 @@ public class SpaceGroupFactoryImplTest {
 			}
 			set.add( t );
 		}
-	}*/
+	}
 
 	private static boolean verbose = true;
 	
@@ -226,465 +226,476 @@ public class SpaceGroupFactoryImplTest {
 		assertEquals( 12, expectedSet.size());
 		return expectedSet;
 
-	}
+	}*/
 
+
+	/**
+	 * return a map from SpaceGroupID to the cardinality of the generators set
+	 *
+	 */
 	private Map<ID,Integer> getMapIDToGeneratorSize() {
 		Map<ID,Integer> ret = new HashMap<ID,Integer>();
-		/*
-		 * script to parse the website:
-		 *
-		 */
 		try {
-			// Group no. 1
+			// 1
 			ret.put( new ID("P1"), 1 );
-			// Group no. 2
+			// 2
 			ret.put( new ID("P-1"), 2  );
-			// Group no. 3
+			// 3
 			ret.put( new ID("P2"), 2  );
-			// Group no. 4
+			// 4
 			ret.put( new ID("P2(1)"), 2  );
-			// Group no. 5
+			// 5
 			ret.put( new ID("C2"), 4  );
-			// Group no. 6
+			// 6
 			ret.put( new ID("Pm"), 2  );
-			// Group no. 7
+			// 7
 			ret.put( new ID("Pc"), 2  );
-			// Group no. 8
+			// 8
 			ret.put( new ID("Cm"), 4  );
-			// Group no. 9
+			// 9
 			ret.put( new ID("Cc"), 4  );
-			// Group no. 1
+			// 10
 			ret.put( new ID("P2/m"), 4  );
-			// Group no. 1
+			// 11
 			ret.put( new ID("P2(1)/m"), 4  );
-			// Group no. 1
+			// 12
 			ret.put( new ID("C2/m"), 8  );
-			// Group no. 1
+			// 13
 			ret.put( new ID("P2/c"), 4  );
-			// Group no. 1
+			// 14
 			ret.put( new ID("P2(1)/c"), 4  );
-			// Group no. 1
+			// 15
 			ret.put( new ID("C2/c"), 8  );
-			// Group no. 1
+			// 16
 			ret.put( new ID("P222"), 4  );
-			// Group no. 1
+			// 17
 			ret.put( new ID("P222(1)"), 4  );
-			// Group no. 1
+			// 18
 			ret.put( new ID("P2(1)2(1)2"), 4  );
-			// Group no. 1
+			// 19
 			ret.put( new ID("P2(1)2(1)2(1)"), 4  );
-			// Group no. 2
+			// 20
 			ret.put( new ID("C222(1)"), 8  );
-			// Group no. 2
+			// 21
 			ret.put( new ID("C222"), 8  );
-			// Group no. 2
+			// 22
 			ret.put( new ID("F222"), 16  );
-			// Group no. 2
+			// 23
 			ret.put( new ID("I222"), 8  );
-			// Group no. 2
+			// 24
 			ret.put( new ID("I2(1)2(1)2(1)"), 8  );
-			// Group no. 2
+			// 25
 			ret.put( new ID("Pmm2"), 4  );
-			// Group no. 2
+			// 26
 			ret.put( new ID("Pmc2(1)"), 4  );
-			// Group no. 2
+			// 27
 			ret.put( new ID("Pcc2"), 4  );
-			// Group no. 2
+			// 28
 			ret.put( new ID("Pma2"), 4  );
-			// Group no. 2
+			// 29
 			ret.put( new ID("Pca2(1)"), 4  );
-			// Group no. 3
+			// 30
 			ret.put( new ID("Pnc2"), 4  );
-			// Group no. 3
+			// 31
 			ret.put( new ID("Pmn2(1)"), 4  );
-			// Group no. 3
+			// 32
 			ret.put( new ID("Pba2"), 4  );
-			// Group no. 3
+			// 33
 			ret.put( new ID("Pna2(1)"), 4  );
-			// Group no. 3
+			// 34
 			ret.put( new ID("Pnn2"), 4  );
-			// Group no. 3
+			// 35
 			ret.put( new ID("Cmm2"), 8  );
-			// Group no. 3
+			// 36
 			ret.put( new ID("Cmc2(1)"), 8  );
-			// Group no. 3
+			// 37
 			ret.put( new ID("Ccc2"), 8  );
-			// Group no. 3
+			// 38
 			ret.put( new ID("Amm2"), 8  );
-			// Group no. 3
+			// 39
 			ret.put( new ID("Abm2"), 8  );
-			// Group no. 4
+			// 40
 			ret.put( new ID("Ama2"), 8  );
-			// Group no. 4
+			// 41
 			ret.put( new ID("Aba2"), 8  );
-			// Group no. 4
+			// 42
 			ret.put( new ID("Fmm2"), 16  );
-			// Group no. 4
+			// 43
 			ret.put( new ID("Fdd2"), 16  );
-			// Group no. 4
+			// 44
 			ret.put( new ID("Imm2"), 8  );
-			// Group no. 4
+			// 45
 			ret.put( new ID("Iba2"), 8  );
-			// Group no. 4
+			// 46
 			ret.put( new ID("Ima2"), 8  );
-			// Group no. 4
+			// 47
 			ret.put( new ID("Pmmm"), 8  );
-			// Group no. 4
+			// 48
 			ret.put( new ID("Pnnn"), 8  );
-			// Group no. 4
+			// 49
 			ret.put( new ID("Pccm"), 8  );
-			// Group no. 5
+			// 50
 			ret.put( new ID("Pban"), 8  );
-			// Group no. 5
+			// 51
 			ret.put( new ID("Pmma"), 8  );
-			// Group no. 5
+			// 52
 			ret.put( new ID("Pnna"), 8  );
-			// Group no. 5
+			// 53
 			ret.put( new ID("Pmna"), 8  );
-			// Group no. 5
+			// 54
 			ret.put( new ID("Pcca"), 8  );
-			// Group no. 5
+			// 55
 			ret.put( new ID("Pbam"), 8  );
-			// Group no. 5
+			// 56
 			ret.put( new ID("Pccn"), 8  );
-			// Group no. 5
+			// 57
 			ret.put( new ID("Pbcm"), 8  );
-			// Group no. 5
+			// 58
 			ret.put( new ID("Pnnm"), 8  );
-			// Group no. 5
+			// 59
 			ret.put( new ID("Pmmn"), 8  );
-			// Group no. 6
+			// 60
 			ret.put( new ID("Pbcn"), 8  );
-			// Group no. 6
+			// 61
 			ret.put( new ID("Pbca"), 8  );
-			// Group no. 6
+			// 62
 			ret.put( new ID("Pnma"), 8  );
-			// Group no. 6
+			// 63
 			ret.put( new ID("Cmcm"), 16  );
-			// Group no. 6
+			// 64
 			ret.put( new ID("Cmca"), 16  );
-			// Group no. 6
+			// 65
 			ret.put( new ID("Cmmm"), 16  );
-			// Group no. 6
+			// 66
 			ret.put( new ID("Cccm"), 16  );
-			// Group no. 6
+			// 67
 			ret.put( new ID("Cmma"), 16  );
-			// Group no. 6
+			// 68
 			ret.put( new ID("Ccca"), 16  );
-			// Group no. 6
+			// 69
 			ret.put( new ID("Fmmm"), 32  );
-			// Group no. 7
+			// 70
 			ret.put( new ID("Fddd"), 32  );
-			// Group no. 7
+			// 71
 			ret.put( new ID("Immm"), 16  );
-			// Group no. 7
+			// 72
 			ret.put( new ID("Ibam"), 16  );
-			// Group no. 7
+			// 73
 			ret.put( new ID("Ibca"), 16  );
-			// Group no. 7
+			// 74
 			ret.put( new ID("Imma"), 16  );
-			// Group no. 7
+			// 75
 			ret.put( new ID("P4"), 4  );
-			// Group no. 7
+			// 76
 			ret.put( new ID("P4(1)"), 4  );
-			// Group no. 7
+			// 77
 			ret.put( new ID("P4(2)"), 4  );
-			// Group no. 7
+			// 78
 			ret.put( new ID("P4(3)"), 4  );
-			// Group no. 7
+			// 79
 			ret.put( new ID("I4"), 8  );
-			// Group no. 8
+			// 80
 			ret.put( new ID("I4(1)"), 8  );
-			// Group no. 8
+			// 81
 			ret.put( new ID("P-4"), 4  );
-			// Group no. 8
+			// 82
 			ret.put( new ID("I-4"), 8  );
-			// Group no. 8
+			// 83
 			ret.put( new ID("P4/m"), 8  );
-			// Group no. 8
+			// 84
 			ret.put( new ID("P4(2)/m"), 8  );
-			// Group no. 8
+			// 85
 			ret.put( new ID("P4/n"), 8  );
-			// Group no. 8
+			// 86
 			ret.put( new ID("P4(2)/n"), 8  );
-			// Group no. 8
+			// 87
 			ret.put( new ID("I4/m"), 16  );
-			// Group no. 8
+			// 88
 			ret.put( new ID("I4(1)/a"), 16  );
-			// Group no. 8
+			// 89
 			ret.put( new ID("P422"), 8  );
-			// Group no. 9
+			// 90
 			ret.put( new ID("P42(1)2"), 8  );
-			// Group no. 9
+			// 91
 			ret.put( new ID("P4(1)22"), 8  );
-			// Group no. 9
+			// 92
 			ret.put( new ID("P4(1)2(1)2"), 8  );
-			// Group no. 9
+			// 93
 			ret.put( new ID("P4(2)22"), 8  );
-			// Group no. 9
+			// 94
 			ret.put( new ID("P4(2)2(1)2"), 8  );
-			// Group no. 9
+			// 95
 			ret.put( new ID("P4(3)22"), 8  );
-			// Group no. 9
+			// 96
 			ret.put( new ID("P4(3)2(1)2"), 8  );
-			// Group no. 9
+			// 97
 			ret.put( new ID("I422"), 16  );
-			// Group no. 9
+			// 98
 			ret.put( new ID("I4(1)22"), 16  );
-			// Group no. 9
+			// 99
 			ret.put( new ID("P4mm"), 8  );
-			// Group no. 1
+			// 100
 			ret.put( new ID("P4bm"), 8  );
-			// Group no. 1
+			// 101
 			ret.put( new ID("P4(2)cm"), 8  );
-			// Group no. 1
+			// 102
 			ret.put( new ID("P4(2)nm"), 8  );
-			// Group no. 1
+			// 103
 			ret.put( new ID("P4cc"), 8  );
-			// Group no. 1
+			// 104
 			ret.put( new ID("P4nc"), 8  );
-			// Group no. 1
+			// 105
 			ret.put( new ID("P4(2)mc"), 8  );
-			// Group no. 1
+			// 106
 			ret.put( new ID("P4(2)bc"), 8  );
-			// Group no. 1
+			// 107
 			ret.put( new ID("I4mm"), 16  );
-			// Group no. 1
+			// 108
 			ret.put( new ID("I4cm"), 16  );
-			// Group no. 1
+			// 109
 			ret.put( new ID("I4(1)md"), 16  );
-			// Group no. 1
+			// 110
 			ret.put( new ID("I4(1)cd"), 16  );
-			// Group no. 1
+			// 111
 			ret.put( new ID("P-42m"), 8  );
-			// Group no. 1
+			// 112
 			ret.put( new ID("P-42c"), 8  );
-			// Group no. 1
+			// 113
 			ret.put( new ID("P-42(1)m"), 8  );
-			// Group no. 1
+			// 114
 			ret.put( new ID("P-42(1)c"), 8  );
-			// Group no. 1
+			// 115
 			ret.put( new ID("P-4m2"), 8  );
-			// Group no. 1
+			// 116
 			ret.put( new ID("P-4c2"), 8  );
-			// Group no. 1
+			// 117
 			ret.put( new ID("P-4b2"), 8  );
-			// Group no. 1
+			// 118
 			ret.put( new ID("P-4n2"), 8  );
-			// Group no. 1
+			// 119
 			ret.put( new ID("I-4m2"), 16  );
-			// Group no. 1
+			// 120
 			ret.put( new ID("I-4c2"), 16  );
-			// Group no. 1
+			// 121
 			ret.put( new ID("I-42m"), 16  );
-			// Group no. 1
+			// 122
 			ret.put( new ID("I-42d"), 16  );
-			// Group no. 1
+			// 123
 			ret.put( new ID("P4/mmm"), 16  );
-			// Group no. 1
+			// 124
 			ret.put( new ID("P4/mcc"), 16  );
-			// Group no. 1
+			// 125
 			ret.put( new ID("P4/nbm"), 16  );
-			// Group no. 1
+			// 126
 			ret.put( new ID("P4/nnc"), 16  );
-			// Group no. 1
+			// 127
 			ret.put( new ID("P4/mbm"), 16  );
-			// Group no. 1
+			// 128
 			ret.put( new ID("P4/mnc"), 16  );
-			// Group no. 1
+			// 129
 			ret.put( new ID("P4/nmm"), 16  );
-			// Group no. 1
+			// 130
 			ret.put( new ID("P4/ncc"), 16  );
-			// Group no. 1
+			// 131
 			ret.put( new ID("P4(2)/mmc"), 16  );
-			// Group no. 1
+			// 132
 			ret.put( new ID("P4(2)/mcm"), 16  );
-			// Group no. 1
+			// 133
 			ret.put( new ID("P4(2)/nbc"), 16  );
-			// Group no. 1
+			// 134
 			ret.put( new ID("P4(2)/nnm"), 16  );
-			// Group no. 1
+			// 135
 			ret.put( new ID("P4(2)/mbc"), 16  );
-			// Group no. 1
+			// 136
 			ret.put( new ID("P4(2)/mnm"), 16  );
-			// Group no. 1
+			// 137
 			ret.put( new ID("P4(2)/nmc"), 16  );
-			// Group no. 1
+			// 138
 			ret.put( new ID("P4(2)/ncm"), 16  );
-			// Group no. 1
+			// 139
 			ret.put( new ID("I4/mmm"), 32  );
-			// Group no. 1
+			// 140
 			ret.put( new ID("I4/mcm"), 32  );
-			// Group no. 1
+			// 141
 			ret.put( new ID("I4(1)/amd"), 32  );
-			// Group no. 1
+			// 142
 			ret.put( new ID("I4(1)/acd"), 32  );
-			// Group no. 1
+			// 143
 			ret.put( new ID("P3"), 3  );
-			// Group no. 1
+			// 144
 			ret.put( new ID("P3(1)"), 3  );
-			// Group no. 1
+			// 145
 			ret.put( new ID("P3(2)"), 3  );
-			// Group no. 1
+			// 146
 			ret.put( new ID("R3"), 9  );
-			// Group no. 1
+			// 147
 			ret.put( new ID("P-3"), 6  );
-			// Group no. 1
+			// 148
 			ret.put( new ID("R-3"), 18  );
-			// Group no. 1
+			// 149
 			ret.put( new ID("P312"), 6  );
-			// Group no. 1
+			// 150
 			ret.put( new ID("P321"), 6  );
-			// Group no. 1
+			// 151
 			ret.put( new ID("P3(1)12"), 6  );
-			// Group no. 1
+			// 152
 			ret.put( new ID("P3(1)21"), 6  );
-			// Group no. 1
+			// 153
 			ret.put( new ID("P3(2)12"), 6  );
-			// Group no. 1
+			// 154
 			ret.put( new ID("P3(2)21"), 6  );
-			// Group no. 1
+			// 155
 			ret.put( new ID("R32"), 18  );
-			// Group no. 1
+			// 156
 			ret.put( new ID("P3m1"), 6  );
-			// Group no. 1
+			// 157
 			ret.put( new ID("P31m"), 6  );
-			// Group no. 1
+			// 158
 			ret.put( new ID("P3c1"), 6  );
-			// Group no. 1
+			// 159
 			ret.put( new ID("P31c"), 6  );
-			// Group no. 1
+			// 160
 			ret.put( new ID("R3m"), 18  );
-			// Group no. 1
+			// 161
 			ret.put( new ID("R3c"), 18  );
-			// Group no. 1
+			// 162
 			ret.put( new ID("P-31m"), 12  );
-			// Group no. 1
+			// 163
 			ret.put( new ID("P-31c"), 12  );
-			// Group no. 1
+			// 164
 			ret.put( new ID("P-3m1"), 12  );
-			// Group no. 1
+			// 165
 			ret.put( new ID("P-3c1"), 12  );
-			// Group no. 1
+			// 166
 			ret.put( new ID("R-3m"), 36  );
-			// Group no. 1
+			// 167
 			ret.put( new ID("R-3c"), 36  );
-			// Group no. 1
+			// 168
 			ret.put( new ID("P6"), 6  );
-			// Group no. 1
+			// 169
 			ret.put( new ID("P6(1)"), 6  );
-			// Group no. 1
+			// 170
 			ret.put( new ID("P6(5)"), 6  );
-			// Group no. 1
+			// 171
 			ret.put( new ID("P6(2)"), 6  );
-			// Group no. 1
+			// 172
 			ret.put( new ID("P6(4)"), 6  );
-			// Group no. 1
+			// 173
 			ret.put( new ID("P6(3)"), 6  );
-			// Group no. 1
+			// 174
 			ret.put( new ID("P-6"), 6  );
-			// Group no. 1
+			// 175
 			ret.put( new ID("P6/m"), 12  );
-			// Group no. 1
+			// 176
 			ret.put( new ID("P6(3)/m"), 12  );
-			// Group no. 1
+			// 177
 			ret.put( new ID("P622"), 12  );
-			// Group no. 1
+			// 178
 			ret.put( new ID("P6(1)22"), 12  );
-			// Group no. 1
+			// 179
 			ret.put( new ID("P6(5)22"), 12  );
-			// Group no. 1
+			// 180
 			ret.put( new ID("P6(2)22"), 12  );
-			// Group no. 1
+			// 181
 			ret.put( new ID("P6(4)22"), 12  );
-			// Group no. 1
+			// 182
 			ret.put( new ID("P6(3)22"), 12  );
-			// Group no. 1
+			// 183
 			ret.put( new ID("P6mm"), 12  );
-			// Group no. 1
+			// 184
 			ret.put( new ID("P6cc"), 12  );
-			// Group no. 1
+			// 185
 			ret.put( new ID("P6(3)cm"), 12  );
-			// Group no. 1
+			// 186
 			ret.put( new ID("P6(3)mc"), 12  );
-			// Group no. 1
+			// 187
 			ret.put( new ID("P-6m2"), 12  );
-			// Group no. 1
+			// 188
 			ret.put( new ID("P-6c2"), 12  );
-			// Group no. 1
+			// 189
 			ret.put( new ID("P-62m"), 12  );
-			// Group no. 1
+			// 190
 			ret.put( new ID("P-62c"), 12  );
-			// Group no. 1
+			// 191
 			ret.put( new ID("P6/mmm"), 24  );
-			// Group no. 1
+			// 192
 			ret.put( new ID("P6/mcc"), 24  );
-			// Group no. 1
+			// 193
 			ret.put( new ID("P6(3)/mcm"), 24  );
-			// Group no. 1
+			// 194
 			ret.put( new ID("P6(3)/mmc"), 24  );
-			// Group no. 1
+			// 195
 			ret.put( new ID("P23"), 12  );
-			// Group no. 1
+			// 196
 			ret.put( new ID("F23"), 48  );
-			// Group no. 1
+			// 197
 			ret.put( new ID("I23"), 24  );
-			// Group no. 1
+			// 198
 			ret.put( new ID("P2(1)3"), 12  );
-			// Group no. 1
+			// 199
 			ret.put( new ID("I2(1)3"), 24  );
-			// Group no. 2
+			// 200
 			ret.put( new ID("Pm-3"), 24  );
-			// Group no. 2
+			// 201
 			ret.put( new ID("Pn-3"), 24  );
-			// Group no. 2
+			// 202
 			ret.put( new ID("Fm-3"), 96  );
-			// Group no. 2
+			// 203
 			ret.put( new ID("Fd-3"), 96  );
-			// Group no. 2
+			// 204
 			ret.put( new ID("Im-3"), 48  );
-			// Group no. 2
+			// 205
 			ret.put( new ID("Pa-3"), 24  );
-			// Group no. 2
+			// 206
 			ret.put( new ID("Ia-3"), 48  );
-			// Group no. 2
+			// 207
 			ret.put( new ID("P432"), 24  );
-			// Group no. 2
+			// 208
 			ret.put( new ID("P4(2)32"), 24  );
-			// Group no. 2
+			// 209
 			ret.put( new ID("F432"), 96  );
-			// Group no. 2
+			// 210
 			ret.put( new ID("F4(1)32"), 96  );
-			// Group no. 2
+			// 211
 			ret.put( new ID("I432"), 48  );
-			// Group no. 2
+			// 212
 			ret.put( new ID("P4(3)32"), 24  );
-			// Group no. 2
+			// 213
 			ret.put( new ID("P4(1)32"), 24  );
-			// Group no. 2
+			// 214
 			ret.put( new ID("I4(1)32"), 48  );
-			// Group no. 2
+			// 215
 			ret.put( new ID("P-43m"), 24  );
-			// Group no. 2
+			// 216
 			ret.put( new ID("F4-3m"), 96  );
-			// Group no. 2
+			// 217
 			ret.put( new ID("I-43m"), 48  );
-			// Group no. 2
+			// 218
 			ret.put( new ID("P-43n"), 24  );
-			// Group no. 2
+			// 219
 			ret.put( new ID("F-43c"), 96  );
-			// Group no. 2
+			// 220
 			ret.put( new ID("I-43d"), 48  );
-			// Group no. 2
+			// 221
 			ret.put( new ID("Pm-3m"), 48  );
-			// Group no. 2
+			// 222
 			ret.put( new ID("Pn-3n"), 48  );
-			// Group no. 2
+			// 223
 			ret.put( new ID("Pm-3n"), 48  );
-			// Group no. 2
+			// 224
 			ret.put( new ID("Pn-3m"), 48  );
-			// Group no. 2
+			// 225
 			ret.put( new ID("Fm-3m"), 192  );
+			/*// 226
+			ret.put( new ID("Fm-3c"), );
+			// 227
+			ret.put( new ID("Fd-3m"), );
+			// 228
+			ret.put( new ID("Fd-3c"), );
+			// 229
+			ret.put( new ID("Im-3m"), );
+			// 230
+			ret.put( new ID("Ia-3d"), );*/
 		}
 		catch( InvalidSpaceGroupIDException e) {
 
